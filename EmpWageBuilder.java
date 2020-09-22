fbf6544c80c62076bf790bf94ca51cb936f62449
ashaheed00 <ashaheed00@gmail.com>
2020-09-22 23:51:19 +0530

    Adding into branch UC2

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,14 +1,20 @@
-// Version UC1
+// Version UC2
 
 public class EmpWageBuilder {
 
 	static final short IS_FULL_TIME = 1;
 	static short empCheck;
+	static final int EMP_RATE_PER_HOUR = 20;
+	static short empHours = 0;
+	static int empWage;
 
 	public static void main(String[] args) {
 
 		empCheck = (short) (Math.random() * 10 % 2);
-		System.out.println("Employee is " + (empCheck == 1 ? "Present" : "Absent"));
 
+		if (empCheck == 1)
+			empHours = 8;
+		empWage = empHours * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
