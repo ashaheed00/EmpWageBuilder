568a3c8fa402cd41465cb06c79aeb62b3871a42b
ashaheed00 <ashaheed00@gmail.com>
2020-09-22 23:48:41 +0530

    Adding into branch UC1

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,8 +1,14 @@
-// Start
+// Version UC1
 
 public class EmpWageBuilder {
 
+	static final short IS_FULL_TIME = 1;
+	static short empCheck;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+
+		empCheck = (short) (Math.random() * 10 % 2);
+		System.out.println("Employee is " + (empCheck == 1 ? "Present" : "Absent"));
+
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EmpWageBuilder
+Version UC1
