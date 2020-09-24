9f209dc90f87534819c5e363c4379f30de479ebf
ashaheed00 <ashaheed00@gmail.com>
2020-09-24 13:44:12 +0530

    Adding into branch UC9

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,17 +1,27 @@
-// Version UC8
+// Version UC9
 
-public class EmpWageBuilderMultiCompany {
-	
+public class EmpWageBuilderObject {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	
-	public static int computeEmpWage(String company, int empRatePerHour, 
-									 int numOfWorkingDays, int maxHoursPerMonth) {
-		
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmpWageBuilderObject(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeEmpWage() {
+
 		int empHours = 0, totalWorkingHours = 0, totalWorkingDays = 0;
 
-		while (totalWorkingDays++ < numOfWorkingDays &&
-				totalWorkingHours < maxHoursPerMonth) {
+		while (totalWorkingDays++ < numOfWorkingDays && totalWorkingHours < maxHoursPerMonth) {
 			short empCheck = (short) (Math.random() * 10 % 3);
 
 			switch (empCheck) {
@@ -26,19 +36,24 @@ public class EmpWageBuilderMultiCompany {
 			}
 
 			totalWorkingHours += empHours;
-			System.out.println("Total Working Day: " + totalWorkingDays + 
-								" & Total Working Hours: " + totalWorkingHours);
 		}
-		
-		int totalEmpWage = totalWorkingHours * empRatePerHour;
-		System.out.println("Total Emp Wage for company " + company + ": " + totalEmpWage);
-		
-		return totalEmpWage;
+		System.out.println("Total Working Day: " + totalWorkingDays + " & Total Working Hours: " + totalWorkingHours);
+		totalEmpWage = totalWorkingHours * empRatePerHour;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for company " + company + ": " + totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage("Dexon", 50, 22, 100);
-		System.out.println("***********************************************");
-		computeEmpWage("BlackCap", 150, 18, 110);
+		EmpWageBuilderObject dexon = new EmpWageBuilderObject("Dexon", 50, 22, 100);
+		EmpWageBuilderObject blackCap = new EmpWageBuilderObject("BlackCap", 150, 18, 110);
+		
+		dexon.computeEmpWage();
+		System.out.println(dexon);
+		blackCap.computeEmpWage();
+		System.out.println(blackCap);
 	}
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # EmpWageBuilder
-Version UC8
+Version UC9
